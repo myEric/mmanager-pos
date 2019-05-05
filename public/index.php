4657f238cdea318471f92571cbf228dda87ca32d
myEric <akaffou.eric@me.com>
2019-05-05 16:46:05 +0200

    Updates

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+// Valid PHP Version?
+$minPHPVersion = '7.0';
+if (phpversion() < $minPHPVersion)
+{
+	die("Your PHP version must be {$minPHPVersion} or higher to run CodeIgniter. Current version: " . phpversion());
+}
+unset($minPHPVersion);
+echo 'Welcome to m\'Manager POS';
