a13ff7ada4db321791073dd85fd91dedf55f10da
myEric <akaffou.eric@me.com>
2019-05-05 21:01:53 +0200

    Update

--- public/index.php
+++ public/index.php
@@ -6,4 +6,4 @@ if (phpversion() < $minPHPVersion)
 	die("Your PHP version must be {$minPHPVersion} or higher to run CodeIgniter. Current version: " . phpversion());
 }
 unset($minPHPVersion);
-echo 'Welcome to m\'Manager POS';
+echo 'Welcome to m\'Manager POS and Invoice Management App';
